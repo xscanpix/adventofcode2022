f215acdb196e70022d1593df375e2d247b55b5e3
Svante Nilsson <svante.nilsson@uu.se>
2022-12-02 15:01:29 +0100

    Day 2 - Add part 2 solution

--- day2/program.c
+++ day2/program.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 
+#define CHOICES 3
+
 static const char *FILE_NAME = "input";
+
+int calc_points(const int our_choice, const int their_choice, int points_matrix[CHOICES][CHOICES])
+{
+    // Choice points: Our choice index + 1
+    // Result points: Index into points_matrix
+    return (our_choice) + 1 + points_matrix[our_choice][their_choice];
+}
 
 int main()
 {
     FILE *file = fopen(FILE_NAME, "r");
 
-    int points_matrix[3][3] = // [Your choice][Their choice]
+    int points_matrix[CHOICES][CHOICES] = // [Our choice][Their choice]
         {
+            // R|P|S
             {3, 0, 6}, // Rock
             {6, 3, 0}, // Paper
             {0, 6, 3}  // Scissor
         };
 
     char buf[2] = {0};
-    int total_points = 0;
+    int total_points_part1 = 0;
+    int total_points_part2 = 0;
 
     while (1)
     {
@@ -38,13 +49,33 @@ int main()
             // Read second choice.
             fread(&buf[1], sizeof(char), 1, file);
 
-            // Choice points: Second choice ASCII-value ('X', 'Y' or 'Z') - 'X' + 1
-            // Result points: Index into points_matrix
-            total_points += (buf[1] - 'X') + 1 + points_matrix[buf[1] - 'X'][buf[0] - 'A'];
+            int our_index = buf[1] - 'X';
+            int their_index = buf[0] - 'A';
+
+            total_points_part1 += calc_points(our_index, their_index, points_matrix);
+
+            if (buf[1] == 'X')
+            {
+                // Loosing choice is the previous modular index from their choice.
+                our_index = (their_index + (CHOICES - 1)) % CHOICES;
+            }
+            else if (buf[1] == 'Y')
+            {
+                // Draw means choose their index.
+                our_index = their_index;
+            }
+            else
+            {
+                // Winning choice is the next modular index from their choice.
+                our_index = (their_index + 1) % CHOICES;
+            }
+
+            total_points_part2 += calc_points(our_index, their_index, points_matrix);
         }
     }
 
     fclose(file);
 
-    printf("Day 2 Answer 1: %d\n", total_points);
+    printf("Day 2 Answer 1: %d\n", total_points_part1);
+    printf("Day 2 Answer 2: %d\n", total_points_part2);
 }
