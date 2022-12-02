8185eeafe92c27e4faa2e7e3fef0c8d9ca349dd5
Svante Nilsson <svante.nilsson@uu.se>
2022-12-02 16:07:56 +0100

    Day 1 - Cleanup code

--- day1/program.c
+++ day1/program.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 #define NUM_MAXES 3
-
 static const char *FILE_NAME = "input";
 
-void update_max(int sum, int *current_max_index, int maxes[])
+void update_max(const int sum, int *const current_max_index, int *const maxes)
 {
 	if (sum > maxes[*current_max_index])
 	{ // Sum is greater than current max.
@@ -22,41 +21,40 @@ int main()
 	int number = 0, sum = 0;
 
 	int maxes[NUM_MAXES] = {0}; // "Circular" buffer of maxes.
-	int current_max_index = 0;		  // Index of current max.
+	int current_max_index = 0;	// Index of current max.
 
 	while (1)
 	{
-		if (feof(file))
-		{
-			break;
-		}
-
 		fread(&c, sizeof(char), 1, file);
 
 		if (c == '\n' || feof(file))
-		{ // New number, end of sum or EOF.
-			if (number > 0)
-			{ // Previous number was not last number to sum.
-				sum += number;
-				number = 0;
-			}
-			else
-			{ // Previous number was last number to sum.
+		{
+			if (sum > 0)
+			{
 				update_max(sum, &current_max_index, maxes);
 				sum = 0;
 			}
 
 			if (feof(file))
-			{ // Handle last sum (if no linebreak at the end).
-				update_max(sum, &current_max_index, maxes);
+			{
 				break;
 			}
 		}
+		else if (c >= '0' && c <= '9')
+		{
+			while (c != '\n' && !feof(file))
+			{
+				number *= 10;
+				number += (c - '0');
+				fread(&c, sizeof(char), 1, file);
+			}
+
+			sum += number;
+			number = 0;
+		}
 		else
 		{
-			// Calculate number in base 10.
-			number *= 10;
-			number += (c - '0');
+			break;
 		}
 	}
 
