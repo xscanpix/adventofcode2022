afcc1a383d5426ed10eca093317ac9fa685c6513
Svante Nilsson <svante.nilsson@uu.se>
2022-12-03 14:33:32 +0100

    Day 3 Part 2

--- day3/program.py
+++ day3/program.py
@@ -18,3 +18,24 @@ for bag in bags:
             total_sum += ord(type) - (ord('a') - 1)
 
 print("Day 3 Answer 1: {}".format(total_sum))
+
+# Part 2
+total_sum = 0
+
+def list_chunk(lst, chunk_size):
+    for i in range(0, len(lst), chunk_size):
+        yield lst[i:i + chunk_size]
+
+for group in list_chunk(bags, 3):
+    types_1 = set(group[0].rstrip())
+    types_2 = set(group[1].rstrip())
+    types_3 = set(group[2].rstrip())
+
+    common = list(types_1.intersection(types_2.intersection(types_3)))[0]
+
+    if common.isupper():
+        total_sum += ord(common) - (ord('A') - 27)
+    else:
+        total_sum += ord(common) - (ord('a') - 1)
+
+print("Day 3 Answer 2: {}".format(total_sum))
